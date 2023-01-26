c19e714c984efd692d9a0148f1d03afb4f10f3dc
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-26 09:34:49 +0530

    Initial commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  // const ChatScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.all(
+            9,
+          ),
+          child: Text('This works!'),
+        ),
+        itemCount: 10,
+      ),
+    );
+  }
+}
